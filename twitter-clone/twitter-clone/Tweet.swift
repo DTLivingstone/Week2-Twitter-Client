f874d515ebc41885055ad367d81dd776e99edc2a
David Livingstone <david@david.tl>
2016-06-13 14:57:54 -0700

    built models

---
+++ twitter-clone/twitter-clone/Tweet.swift
@@ -0,0 +1,25 @@
+//
+//  Twitter.swift
+//  twitter-clone
+//
+//  Created by David Livingstone on 6/13/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+import Foundation
+
+class Tweet {
+    let text: String
+    let id: String
+    let user: User?
+    
+    init?(json: [String : AnyObject]) {
+        if let text = json["text"] as? String, id = json["id_str"] as? String, user = json["user"] as? [String: AnyObject] {
+            self.text = text
+            self.id = id
+            self.user = User(json: user)
+        } else {
+            return nil
+        }
+    }
+}
