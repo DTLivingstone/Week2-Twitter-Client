8146f905e59839f6a530729cd4b18376526faea3
David Livingstone <david@david.tl>
2016-06-14 13:40:05 -0700

    started api file

---
+++ twitter-clone/twitter-clone/API.swift
@@ -0,0 +1,18 @@
+//
+//  API.swift
+//  twitter-clone
+//
+//  Created by David Livingstone on 6/14/16.
+//  Copyright © 2016 David Livingstone. All rights reserved.
+//
+
+import Foundation
+import Accounts
+import Social
+
+private func login(completion: (account: ACAccount?) -> ()) {
+    let accountStore
+    let accountType
+    
+    
+}
